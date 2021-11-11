ca82c8f97e479821edc358c53b4d2b5e5117abb3
Pavel <85952748+Polachello@users.noreply.github.com>
2021-11-11 21:25:45 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import Counter from './Counter';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
